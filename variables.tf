006d1e9410a5742ba4262ebaaf307864ec248c15
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-04 22:49:56 +0530

    storage acct variables declaration

--- variables.tf
+++ variables.tf
@@ -1,30 +1,39 @@
-    
-variable "vnet-name" {
-    type = string
-    default = "vnet1"
-}
-variable "vnet-ip" {
-    type = string
-    default = "10.0.0.0/16"
-}
-variable "subnet" {
-    type = string
-    default = "subnet1"
-}
-variable "subnet-ip" {
-    type = string
-    default = "10.0.0.0/24"
-}
-variable "nic-name" {
-    type = string
-    default = "nic1"
-}
-variable "vm-name" {
-    type = string
-    default = "vm1"
-}
-variable "vm-password" {
-    type = string
-    default = "Newyear2026"
-    sensitive = true
+
+variable "storage-acct" {
+    type = map(object({
+      name = string
+      replication = string
+      tags =  map(string)
+    }))
+
+    default = {
+      "storage1" = {
+        name = "bmwstorage01"
+        replication = "LRS"
+        tags = {
+            "owner" = "kunal yeole"
+            "use" = "spare parts logs"
+        }     
+      }
+
+      "storage2" = {
+        name = "bmwstorage02"
+        replication = "ZRS"
+        tags = {
+            "owner" = "kunal yeole"
+            "use" = "sales logs"
+        } 
+      }
+
+      "storage3" = {
+        name = "bmwstorage03"
+        replication = "ZRS"
+        tags = {
+            "owner" = "kunal yeole"
+            "use" = " data analystics logs"
+        }
+      }
+
+    }
+  
 }
