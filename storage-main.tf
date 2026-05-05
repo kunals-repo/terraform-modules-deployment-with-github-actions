be64f1b746622dce124304fa6311b7fad1dfd4d8
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-05 07:33:42 +0530

    containers , blobs creation

--- storage-main.tf
+++ storage-main.tf
@@ -10,5 +10,16 @@ module "storageacct1" {
     rg-location = azurerm_resource_group.storage-rg.location
     storage-name = var.storage-name
     replication-type = var.replication-type
-  
+}
+module "storageacct1-container1" {
+  source = "./Modules/Containers"
+  container-name = var.container-name
+  storageaccount-id = module.storageacct1.storageacct-id
+}
+module "storageacct1-container1-blob1" {
+  source = "./Modules/Blobs"
+  blob-name = var.blob-name
+  storageacct-name = module.storageacct1.storageacct-name
+  container-name = module.storageacct1-container1.container-name
+  blob-source = var.blob-source
 }
