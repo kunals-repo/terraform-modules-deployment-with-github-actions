39cb64cfd81c191e3696f3a77763deec3735ec3c
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-05 06:00:01 +0530

    variables.tf commit

--- variables.tf
+++ variables.tf
@@ -1,30 +1,7 @@
 
-variable "storage-acct" {
-    type = map(object({
-      name = string
-      replication = string
-    
-    }))
-
-    default = {
-      "storage1" = {
-        name = "bmwstorage01"
-        replication = "LRS"
-         
-      }
-
-      "storage2" = {
-        name = "bmwstorage02"
-        replication = "ZRS"
-        
-      }
-
-      "storage3" = {
-        name = "bmwstorage03"
-        replication = "ZRS"
- 
-      }
-
-    }
-  
+variable "storage-name" {
+    type = string
+}
+variable "replication-type" {
+    type = string
 }
