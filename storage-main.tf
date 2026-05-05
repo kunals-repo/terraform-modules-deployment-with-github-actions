6d2f8bf4b7ab4de174ed1ed61a64a0aedb9787e7
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-05 20:07:59 +0530

    third storage acct creation

--- storage-main.tf
+++ storage-main.tf
@@ -40,3 +40,25 @@ module "storageacct2" {
     storage-name = var.storage-name2
     replication-type = var.replication-type2
 }
+
+// third storage account creation ==
+
+module "storageacct3" {
+    source = "./Modules/StorageAcct"
+    rg-name = azurerm_resource_group.storage-rg.name
+    rg-location = azurerm_resource_group.storage-rg.location
+    storage-name = var.storage-name3
+    replication-type = var.replication-type3
+}
+module "storageacct3-container2" {
+  source = "./Modules/Containers"
+  container-name = var.container-name2
+  storageaccount-id = module.storageacct1.storageacct-id
+}
+module "storageacct3-container2-blob2" {
+  source = "./Modules/Blobs"
+  blob-name = var.blob-name2
+  storageacct-name = module.storageacct1.storageacct-name
+  container-name = module.storageacct1-container1.container-name
+  blob-source = var.blob-source2
+}
